9754e998ee09b302169e463e006e8ee9291272f2
cvernerie <verneriecamille@gmail.com>
2018-02-25 17:10:39 +0100

    feat(deals): get deals from lafourchette

--- js/lafourchette.js
+++ js/lafourchette.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 const config = require('../config/lafourchette.json');
 
 const michelinRestaurants = require('../data/restaurants')
+const lafourchetteRestaurants = require('../data/lafourchetteRestaurantsIds')
 
 module.exports = {
 
@@ -13,8 +14,44 @@ module.exports = {
         for(i = 0; i < michelinRestaurants.length; i++) {
             getLaFourchetteRestaurant(michelinRestaurants[i])
         }
+    },
+
+    findDeals: function (callback) {
+        for(i = 0; i < lafourchetteRestaurants.length; i++) {
+            getLafourchetteDeals(lafourchetteRestaurants[i])
+        }
     }
 };
+
+function getLafourchetteDeals(lafourchetteRestaurant) {
+    var searchUrl = encodeURI(config.apiHost + config.dealUrl.replace("%s", lafourchetteRestaurant.id))
+
+    request(searchUrl, function (error, response, html) {
+
+        if (!error && response.statusCode === 200) {
+
+            var jsonResults = JSON.parse(response.body)
+
+            var deals = []
+
+            for(j = 0; j < jsonResults.length; j++) {
+                if (jsonResults[j].is_special_offer) {
+                    deals.push(jsonResults[j])
+                }
+            }
+
+            fs.appendFile('./lafourchetteDeals.json',
+                '{ "restaurantId": '
+                + lafourchetteRestaurant.id
+                + ', "deals": '
+                + JSON.stringify(deals)
+                + '},\n')
+
+        } else {
+            console.log("error : " + searchUrl)
+        }
+    })
+}
 
 function getLaFourchetteRestaurant(michelinRestaurant) {
 
